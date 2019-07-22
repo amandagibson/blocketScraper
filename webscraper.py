82ccec16462290c979146d91e042ac3e7ab10a49
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-07-22 17:40:18 +0200

    pulls all articles with the specified class

--- webscraper.py
+++ webscraper.py
@@ -5,4 +5,6 @@ url = 'https://www.blocket.se/goteborg?ca=15'
 response = requests.get(url)
 content = BeautifulSoup(response.content, "html.parser")
 
-print(content)
+advertismentList = content.find_all('article', attrs={'class': 'media item_row ptm pbm nmt'})
+
+print(advertismentList)
