c920cccd13d593945176780da3cbeac5f67c5023
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-07-23 15:43:48 +0200

    updates object format, sucessfully pulling title and price text in an array

--- webscraper.py
+++ webscraper.py
@@ -5,10 +5,13 @@ url = 'https://www.blocket.se/goteborg?ca=15'
 response = requests.get(url)
 content = BeautifulSoup(response.content, "html.parser")
 
-# to print a single 'price'
-# prices = content.find('p', attrs={'class': 'list_price font-large'})
-
-# print(prices)
-
-for prices in content.findAll('p', attrs={'class': 'list_price font-large'}):
-  print(prices.text.encode('utf-8'))
+advertArr = []
+for advert in content.findAll('article', attrs={"itemtype": "http://schema.org/Offer"}):
+  advertObject = {
+    "title": advert.find('a', attrs={"class": "item_link"}).text,
+    "price": advert.find('p', attrs={"class": "list_price font-large"}).text,
+    # "ad_url": advert.find('a', attrs={"itemprop": "url"}),
+    # "description": advert.find('a', attrs={"title": }),
+    # "photos_url": advert.find('', attrs{})
+  }
+print(advertObject)
